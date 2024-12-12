789ad2e177314475d139003ed63142771f71a0ac
antoine-perfettini <antoine.perfettini@edu.esiee.fr>
2024-12-12 12:25:20 +0000

    mon code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#### Imports et définition des variables globales
-import random
-
+"""
+Code principal
+"""
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
@@ -14,39 +14,71 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
-    return l
+    with open(filename, mode = 'r', encoding='utf8') as f:
+        l = f.readlines()
+        out = []
+        for elt in l:
+            l1 = []
+            for i in  elt.strip().split(';'):
+                l1.append(int(i))
+            out.append(l1)
+    return out
 
 def get_list_k(data, k):
-    l = []
+    """
+    Permet obtenir une liste d'indice k
+    """
+    l = data[k]
     return l
 
 def get_first(l):
-    return None
+    """
+    Permet d'obtenir le premier element de la liste l
+    """
+    return l[0]
 
 def get_last(l):
-    return None
+    """
+    Permet d'obtenir le dernier element de la liste l
+    """
+    return l[-1]
 
 def get_max(l):
-    return None
+    """
+    Permet d'obtenir l'element de valeur maximum de la liste l
+    """
+    return max(l)
 
 def get_min(l):
-    return None
+    """
+    Permet d'obtenir l'element de valeur minimum de la liste l
+    """
+    return min(l)
 
 def get_sum(l):
-    return None
-
-
-#### Fonction principale
-
+    """
+    Permet d'obtenir la somme des elements de la liste l
+    """
+    s = 0
+    for elt in l:
+        s+=elt
+    return s
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    """
+    #### Fonction principale
+    """
+    data = read_data(FILENAME)
+    print(data)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_list_k(data, 37))
+    print(get_first(get_list_k(data, 37)))
+    print(get_last(get_list_k(data, 37)))
+    print(get_max(get_list_k(data, 37)))
+    print(get_min(get_list_k(data, 37)))
+    print(get_sum(get_list_k(data, 37)))
 
 
 if __name__ == "__main__":
